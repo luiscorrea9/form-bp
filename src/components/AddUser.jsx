ae1aaf06d5416cf70a8ea248c1d6269ec577f054
Luis Correa <Luiscorrea874@gmail.com>
2022-10-13 19:30:29 -0500

    first commit

---
+++ src/components/AddUser.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AddUser = () => {
+
+
+  return (
+    
+    <div>
+      </div>
+  
+    )
+}
+
+export default AddUser
---
+++ src/components/EditUser.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const EditUser = (props) => {
+
+    
+    
+  
+
+  return (
+   
+    <div>
+      </div>
+  
+    )
+}
+
+export default EditUser;
